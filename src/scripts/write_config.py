9bd3c5a216bd323c86e80a1f20f3c309c3323966
Dan Seremet <seremet.dan@gmail.com>
2019-04-28 20:31:07 +0100

    writing correlated errors huge config

--- src/scripts/write_config.py
+++ src/scripts/write_config.py
@@ -18,39 +18,49 @@ def write_option(fout, name, value):
     fout.write(PARAM_STR.format(name, value))
 
 ################ change these ################
-FILENAME = "rand.txt"
-N_DIFF_SHAPES = 30
-N_REPEAT = 10
+FILENAME = "corr_big.txt"
 
-NX, NY = 2**13, 2**13
-DATA_PREFIX = os.path.join(DATA_DIR, "rand")
-TASKS = "params fwhp central_amplitude"
-REL_SENS, ABS_SENS = 0, 0
-N_SHAPES = N_DIFF_SHAPES * N_REPEAT
+# for sigma put 6 values between 0 and pi/2 and two between pi/2 and pi
+N_SIGMAS = 12
+sigmas = [np.pi / 2 / (N_SIGMAS-2) * (i+1) for i in range(N_SIGMAS-2)] + [4 * np.pi / 6, 5 * np.pi / 6]
+print(sigmas)
+# for correlation length put 20 values uniformly from 0 to 3.0
+N_LS = 20
+ls = [3 / N_LS * (i+1) for i in range(N_LS)]
+print(ls)
 
-TYPE = "rand_errors"
+# repeat each this many times - to deal with the randomness
+N_DIFF_SHAPES = len(ls) * len(sigmas)
+N_REPEAT = 5
+
+NX, NY = 2**12, 2**12
+DATA_PREFIX = os.path.join(DATA_DIR, "corr_big")
+TASKS = "params fwhp fwhp_y central_amplitude out_lims"
+REL_SENS, ABS_SENS = 0.01, 0
+
+TYPE = "corr_errors"
 LX, LY = 512, 512
 
 ################ leave this ################
 with open(os.path.join(CONF_DIR, FILENAME), "w") as fout:
-    write_option(fout, "nx", 2**13)
-    write_option(fout, "ny", 2**13)
+    write_option(fout, "nx", NX)
+    write_option(fout, "ny", NY)
     write_option(fout, "prefix", DATA_PREFIX)
     write_option(fout, "tasks", TASKS)
     write_option(fout, "rel_sens", REL_SENS)
     write_option(fout, "abs_sens", ABS_SENS)
-    write_option(fout, "n_shapes", N_SHAPES)
+    write_option(fout, "n_shapes", N_DIFF_SHAPES * N_REPEAT)
 
-    for i in range(N_DIFF_SHAPES):
-        for j in range(N_REPEAT):
-            fout.write("\n")
-            write_option(fout, "type", TYPE)
-            write_option(fout, "lx", LX)
-            write_option(fout, "ly", LY)
-            ################ and change this: ################
-            r = 6           # telescope radius
-            sig = 3         # taper
-            r_int = 0       # interior hole
-            s_eps = np.pi / N_DIFF_SHAPES * (i+1)        # noise std dev
-            seed = np.random.randint(1, 32e3)            # rng seed
-            write_option(fout, "params", "{: 5.5f} {: 5.5f} {: 5.5f} {: 5.5f} {: d}".format(r, sig, r_int, s_eps, seed))
+    for s_eps in sigmas:
+        for l_corr in ls:
+            for _ in range(N_REPEAT):
+                fout.write("\n")
+                write_option(fout, "type", TYPE)
+                write_option(fout, "lx", LX)
+                write_option(fout, "ly", LY)
+                ################ and change this: ################
+                r = 6           # telescope radius
+                sig = 3         # taper
+                r_int = 0       # interior hole
+                seed = np.random.randint(1, 32e3)        # rng seed
+                write_option(fout, "params", "{: 5.5f} {: 5.5f} {: 5.5f} {: 5.5f} {:05d} {: 5.5f}".format(r, sig, r_int, s_eps, seed, l_corr))
